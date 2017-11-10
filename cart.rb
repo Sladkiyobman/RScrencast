1c43bd690a49269fa3d07d8c72e381f6209fc823
Sladkiyobman <masliy.grishka@gmail.com>
2017-11-10 09:21:07 +0200

    sc 32

--- cart.rb
+++ cart.rb
@@ -5,6 +5,9 @@ class Cart
 include ItemContainer
   class ItemNotSupported < StandardError; end
 
+
+  UNSUPOPORTED_ITEMS = [AntiqueItem ,VirtualItem]
+
   def initialize(owner)
     @items = Array.new
     @owner = owner
@@ -13,7 +16,7 @@ include ItemContainer
   def save_to_file
     File.open("#{@owner}_cart.txt", "w") do |f|
       @items.each do |i|
-        raise ItemNotSupported if i.class == VirtualItem
+        raise ItemNotSupported if UNSUPOPORTED_ITEMS.include?(i.class)
         f.puts i
       end
     end
